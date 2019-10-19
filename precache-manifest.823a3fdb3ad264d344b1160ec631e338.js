868a96072b364fb908f628ff7619f9d0a73c3c54
Udeep Shrestha <udeepbahadur7@gmail.com>
2019-10-19 17:03:01 +0545

    Updates

---
+++ precache-manifest.823a3fdb3ad264d344b1160ec631e338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5b69c5a61c16528a6091373a3cae01",
+    "url": "/useForm/index.html"
+  },
+  {
+    "revision": "f24b41308d48537fde53",
+    "url": "/useForm/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4d9fa5405b88abad53c1",
+    "url": "/useForm/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f24b41308d48537fde53",
+    "url": "/useForm/static/js/2.d0c6476a.chunk.js"
+  },
+  {
+    "revision": "4d9fa5405b88abad53c1",
+    "url": "/useForm/static/js/main.625dccfa.chunk.js"
+  },
+  {
+    "revision": "edd42f1786ddd4358cbd",
+    "url": "/useForm/static/js/runtime-main.61601cd5.js"
+  }
+]);
